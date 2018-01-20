ff8cdab4de0130b598a55b075370b4d4f123e4ca
vagrant <vagrant@homestead>
2018-01-20 12:56:31 +0000

    finish user auth

--- routes/web.php
+++ routes/web.php
@@ -13,3 +13,19 @@
 
 // name 可以为路由指定名称，命名路由可以方便地为指定路由生成 URL 或者重定向
 Route::get('/', 'PagesController@root')->name('root');
+
+// Authentication Routes...
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login');
+Route::post('logout', 'Auth\LoginController@logout')->name('logout');
+
+// Registration Routes...
+Route::get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
+Route::post('register', 'Auth\RegisterController@register');
+
+// Password Reset Routes...
+Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+Route::post('password/reset', 'Auth\ResetPasswordController@reset');
+
