eeefad965f4d10b9cf70cb7f9a796a5e1f721544
vagrant <vagrant@homestead>
2018-03-02 06:21:56 +0000

    Add comment of helpers

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -1,5 +1,10 @@
 <?php
-
+/**
+ * 根据当前路由返回对应的样式类名
+ * 
+ * @example users.edit => user-edit
+ * @return string
+ */
 function route_class()
 {
     return str_replace('.', '-', Route::currentRouteName());
