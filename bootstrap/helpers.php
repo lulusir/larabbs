72d2c6463ccfba6f5c7be122281a67406b1357c5
vagrant <vagrant@homestead>
2018-03-16 08:26:09 +0000

    User can create topic

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -1,11 +1,17 @@
 <?php
 /**
  * 根据当前路由返回对应的样式类名
- * 
+ *
  * @example users.edit => user-edit
  * @return string
  */
 function route_class()
 {
     return str_replace('.', '-', Route::currentRouteName());
+}
+
+function make_excerpt($value, $length = 200)
+{
+    $excerpt = trim(preg_replace('/\r\n|\r|\n+/', ' ', strip_tags($value)));
+    return str_limit($excerpt, $length);
 }
