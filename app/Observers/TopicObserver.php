5d37e0ec8f88d78c27af8867ec8f51992b10895f
vagrant <vagrant@homestead>
2018-04-18 03:57:36 +0000

    Auto translate slug

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -3,6 +3,7 @@
 namespace App\Observers;
 
 use App\Models\Topic;
+use App\Handlers\SlugTranslateHandler;
 
 // creating, created, updating, updated, saving,
 // saved,  deleting, deleted, restoring, restored
@@ -24,6 +25,13 @@ class TopicObserver
         // 使用HTMLPurifier for Laravel 5的clean方法过滤非法标签，防止xss（跨站脚本攻击）
         // 设置在config/purifier.php
         $topic->body = clean($topic->body, 'user_topic_body');
+
+        // 生成话题摘抄
         $topic->excerpt = make_excerpt($topic->body);
+
+        // 如 slug 字段无内容，即使用翻译器对 title 进行翻译
+        if ( ! $topic->slug) {
+            $topic->slug = app(SlugTranslateHandler::class)->translate($topic->title);
+        }
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -30,7 +30,9 @@ Route::post('password/reset', 'Auth\ResetPasswordController@reset');
 
 Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
 
-Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'create', 'store', 'update', 'edit', 'destroy']]);
+// 使用slug也能访问到show页面
+Route::get('topics/{topic}/{slug?}', 'TopicsController@show')->name('topics.show');
 
 Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
 
